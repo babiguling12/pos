5ee567533d63cae585f03ba6e552fb9c7612285c
babiguling12 <sudarmaputra277@gmail.com>
2024-07-04 11:08:54 +0800

    yaya

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ include '../include/include.php';
     <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
     <meta name="author" content="Łukasz Holeczek">
     <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-    <title>CoreUI Free Bootstrap Admin Template</title>
+    <title>Toko Anjay</title>
     <link rel="apple-touch-icon" sizes="57x57" href="../assets/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="../assets/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="../assets/favicon/apple-icon-72x72.png">
