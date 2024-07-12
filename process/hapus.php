d988563d5aad6e9dddeea801a228083d4b15e784
babiguling12 <sudarmaputra277@gmail.com>
2024-07-12 11:10:26 +0800

    hahahaha

--- process/hapus.php
+++ process/hapus.php
@@ -4,6 +4,8 @@ include '../include/include.php';
 
 if(isset($_GET['s']) && $_GET['s'] === 'pembeli') hapuspembeli($_GET['id']);
 
+if(isset($_GET['s']) && $_GET['s'] === 'pengguna') hapuspengguna($_GET['id']);
+
 
 
 
@@ -25,5 +27,19 @@ function hapuspembeli($id) {
         header("Location: ../public/pembeli.php");
     } else {
         header("Location: ../public/pembeli.php?err=1");
+    }
+}
+
+function hapuspengguna($id) {
+    global $conn;
+
+    $query  = "DELETE FROM pengguna WHERE id_pengguna = $id";
+
+    mysqli_query($conn, $query);
+
+    if(!mysqli_errno($conn)) {
+        header("Location: ../public/pengguna.php");
+    } else {
+        header("Location: ../public/pengguna.php?err=1");
     }
 }
--- process/tambah.php
+++ process/tambah.php
@@ -47,16 +47,6 @@ function tambahpengguna($data) {
 
     $query  = "INSERT INTO pengguna VALUES (NULL, '$username', '$password', '$nama', '$jabatan', '$nohp')";
 
-    $username   = htmlspecialchars($data['user_name']);
-    $password        = htmlspecialchars($data['password']);
-    $nama       = htmlspecialchars($data['nama_pengguna']);
-    $jabatan     = htmlspecialchars($data['jabatan']);
-    $nomor      = htmlspecialchars($data['nohp_pengguna']);
-
-    $password = password_hash($password, PASSWORD_DEFAULT);
-
-    $query  = "INSERT INTO pengguna VALUES (NULL, '$username', '$password', '$nama', '$jabatan', '$nomor')";
-
     mysqli_query($conn, $query);
 
     if(!mysqli_errno($conn)) {
