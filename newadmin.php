47a1c24812d6f2bd9941d7c6ef57a6a67e6d7395
babiguling12 <sudarmaputra277@gmail.com>
2024-07-19 02:39:18 +0800

    sad bgt anjing

--- newadmin.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include "include/database.php";
-$password = password_hash("admin", PASSWORD_DEFAULT);
-
-mysqli_query($conn, "INSERT INTO pengguna VALUES (NULL, 'admin', '$password', 'Admin', 'admin', '08admin')");
